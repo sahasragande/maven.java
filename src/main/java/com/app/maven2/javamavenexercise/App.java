4cb4768d6c11f6d15c2d50342cf5b332fb05b199
sahasra <sahasragande66@gmail.com>
2024-11-14 15:57:44 +0530

    add maven.java files

---
+++ src/main/java/com/app/maven2/javamavenexercise/App.java
@@ -0,0 +1,10 @@
+package com.app.maven2.javamavenexercise;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Sahasra,22BD1A054K");
+    }
+}
